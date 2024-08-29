ddc94a6efed63eca2f00000b4ce9dcf803534b6e
siarheima-papaya <siarheima@papayaglobal.com>
2024-08-29 12:09:20 +0200

    Add CRU methods in AccountMovie

--- src/main/java/com/jd2/moviebase/service/AccountMovieService.java
+++ src/main/java/com/jd2/moviebase/service/AccountMovieService.java
@@ -1,8 +1,11 @@
 package com.jd2.moviebase.service;
 
+import com.jd2.moviebase.model.AccountMovie;
 import com.jd2.moviebase.repository.AccountMovieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class AccountMovieService {
     private static final Logger logger = LoggerFactory.getLogger(AccountMovieService.class);
@@ -10,6 +13,21 @@ public class AccountMovieService {
 
     public AccountMovieService(AccountMovieRepository accountMovieRepository) {
         this.accountMovieRepository = accountMovieRepository;
+    }
+
+    public AccountMovie create(AccountMovie accountMovie) {
+        logger.info("Creating account movie: {}", accountMovie);
+        return accountMovieRepository.create(accountMovie);
+    }
+
+    public List<AccountMovie> findAllByAccountId(int accountId) {
+        logger.info("Finding all account movies by account id: {}", accountId);
+        return accountMovieRepository.findAllByAccountId(accountId);
+    }
+
+    public void updateStatusByAccId(int accountId, int movieId, String status) {
+        logger.info("Updating account movie status by account id: {}", accountId);
+        accountMovieRepository.updateStatusByAccId(accountId, movieId, status);
     }
 
     public void deleteByAccId(int id) {
