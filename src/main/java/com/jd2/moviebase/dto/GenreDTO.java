10b7f9628796b66a5115fead1db757934ac060b9
PavelKazukhouski <p.kazhukhouski@gmail.com>
2024-09-25 22:39:54 +0200

    add GenreDTO

---
+++ src/main/java/com/jd2/moviebase/dto/GenreDTO.java
@@ -0,0 +1,10 @@
+package com.jd2.moviebase.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+public class GenreDTO {
+}
