6edf2f7e031a81c054ba6ca905efcc77aee3bcf6
siarheima-papaya <siarheima@papayaglobal.com>
2024-10-09 11:16:21 +0200

    Add SecurityWebApplicationInitializer

---
+++ src/main/java/com/jd2/moviebase/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.jd2.moviebase.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
