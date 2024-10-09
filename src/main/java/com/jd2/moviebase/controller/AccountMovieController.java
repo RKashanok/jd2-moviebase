b4e36a297194d98ca3ac3b2e196a73862a6b1891
RKashanok <52324519+RKashanok@users.noreply.github.com>
2024-10-09 13:21:47 +0200

    updates (#26)
    * updates
    
    * updates v2

--- src/main/java/com/jd2/moviebase/controller/AccountMovieController.java
+++ src/main/java/com/jd2/moviebase/controller/AccountMovieController.java
@@ -3,14 +3,21 @@ package com.jd2.moviebase.controller;
 import com.jd2.moviebase.dto.AccountMovieDto;
 import com.jd2.moviebase.service.AccountMovieService;
 import com.jd2.moviebase.util.ConstantsHelper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class AccountMovieController {
+
     private AccountMovieService accountMovieService;
 
     @Autowired
@@ -30,8 +37,9 @@ public class AccountMovieController {
 
     // Обновление статуса account_movie по accountId и movieId
     @PutMapping("/account-movies/{accountId}/{movieId}")
-    public void updateMovieStatus(@PathVariable(value = "accountId") int accountId, @PathVariable(value = "movieId") int movieId,
-                                  @RequestBody ConstantsHelper.MovieStatus status) {
+    public void updateMovieStatus(@PathVariable(value = "accountId") int accountId,
+        @PathVariable(value = "movieId") int movieId,
+        @RequestBody ConstantsHelper.MovieStatus status) {
         accountMovieService.updateStatusByAccId(accountId, movieId, status);
     }
 
