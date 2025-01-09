1f0bd49587e27894118d40e796a2e241bdc0dae5
siarheima-papaya <siarheima@papayaglobal.com>
2025-01-09 21:10:26 +0100

    Minor changes with import

--- src/main/java/com/jd2/moviebase/model/User.java
+++ src/main/java/com/jd2/moviebase/model/User.java
@@ -1,20 +1,14 @@
 package com.jd2.moviebase.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
-
 
 @Data
 @Builder
