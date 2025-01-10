699f1d518269ffdb7621c101c0010c307a59a607
siarheima-papaya <siarheima@papayaglobal.com>
2025-01-10 09:59:20 +0100

    Remove logback from pom.xml; move annotation @EnableJpaRepositories

--- src/main/java/com/jd2/moviebase/MovieBaseApplication.java
+++ src/main/java/com/jd2/moviebase/MovieBaseApplication.java
@@ -2,10 +2,10 @@ package com.jd2.moviebase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RestController
+@EnableJpaRepositories
 public class MovieBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieBaseApplication.class, args);
--- src/main/java/com/jd2/moviebase/config/WebConfig.java
+++ src/main/java/com/jd2/moviebase/config/WebConfig.java
@@ -3,12 +3,10 @@ package com.jd2.moviebase.config;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
-@EnableJpaRepositories
 public class WebConfig {
     @Bean
     public OkHttpClient okHttpClient() {
