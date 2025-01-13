1c8bbf90ab72307ee10999a050f32a83a287dd9d
PavelKazukhouski <p.kazhukhouski@gmail.com>
2025-01-13 15:15:38 +0100

    Edit AccountMovieRepository

--- src/main/java/com/jd2/moviebase/repository/AccountMovieRepository.java
+++ src/main/java/com/jd2/moviebase/repository/AccountMovieRepository.java
@@ -1,19 +1,13 @@
 package com.jd2.moviebase.repository;
 
-import com.jd2.moviebase.exception.MovieDbRepositoryOperationException;
 import com.jd2.moviebase.model.AccountMovie;
 import jakarta.transaction.Transactional;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-import static com.jd2.moviebase.util.ConstantsHelper.MovieStatus;
 
 @Repository
 public interface AccountMovieRepository extends JpaRepository<AccountMovie, Long> {
